f25e0d7a2c3d64ba3b115d0f5d684f0f75441763
solbell2 <kelseysun13@gmail.com>
2023-10-15 11:01:01 -0700

    Added log of user drinks

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
 from flask_migrate import Migrate
+from datetime import datetime
 
 # Create a Flask application
 app = Flask(__name__)
 app.secret_key = '6969'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # SQLite database file
 db = SQLAlchemy(app)
-with app.app_context():
-    db.create_all()
+login_manager = LoginManager(app)
 migrate = Migrate(app, db)
+
+login_manager.init_app(app)
+login_manager.login_view = 'login'  # Specify the login route
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class UserDrinks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    drink_type = db.Column(db.String(255), nullable=False)
+    drink_size = db.Column(db.String(255), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Define a relationship to the User model
+    user = db.relationship('User', back_populates='drinks', lazy=True)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,13 +40,9 @@ class User(UserMixin, db.Model):
         self.password = password
         self.caffeine = 0
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'  # Specify the login route
+    # Define the one-to-many relationship to drinks
+    drinks = db.relationship('UserDrinks', back_populates='user', lazy=True, cascade='all, delete-orphan')
 
-# Define a user loader function
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 from app import routes
+
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, session, redirect, url_for, request, flash
 from flask_login import login_user, login_required, login_manager, logout_user
-from app import app, coffee_data, db, User
+from app import app, coffee_data, db, User, UserDrinks
+from datetime import datetime
 
 # Define a route for the homepage
 @app.route('/')
@@ -53,21 +54,30 @@ def register():
 
     return render_template('register.html')
 
-# Define a route for counter
 @app.route('/counter/<int:user_id>')
 @login_required
 def counter(user_id):
-    hot_cold_options = ["Hot", "Cold"]
-    drink_type_options = [
-        "Americano", "Latte", "Flat white", "Cold brew", "Shaken espresso", 
-        "Brewed coffee", "Brewed pike", "Brewed dark", "Chai", 
-        "Black tea", "Green tea", "Passion tea", "Refresher", 
-        "Frappuccino", "Creme Frappuccino"
-    ]
-    drink_size_options = ["Short", "Tall", "Grande", "Venti", "Trenta"]
+    # Fetch the user based on user_id
     user = User.query.get(user_id)
 
-    return render_template('counter.html', user_id=user.id, counter=user.caffeine, hot_cold_options=hot_cold_options, drink_type_options=drink_type_options, drink_size_options=drink_size_options)
+    if user is not None:
+        hot_cold_options = ["Hot", "Cold"]
+        drink_type_options = [
+            "Americano", "Latte", "Flat white", "Cold brew", "Shaken espresso", 
+            "Brewed coffee", "Brewed pike", "Brewed dark", "Chai", 
+            "Black tea", "Green tea", "Passion tea", "Refresher", 
+            "Frappuccino", "Creme Frappuccino"
+        ]
+        drink_size_options = ["Short", "Tall", "Grande", "Venti", "Trenta"]
+        
+        # Query the user's recent drinks
+        recent_drinks = user.drinks[-10:]  # Get the last 10 drinks
+
+        return render_template('counter.html', user=user, user_id=user.id, counter=user.caffeine, hot_cold_options=hot_cold_options, drink_type_options=drink_type_options, drink_size_options=drink_size_options, recent_drinks=recent_drinks)
+    else:
+        flash('User not found', 'error')
+        return redirect(url_for('homepage'))
+
 
 @app.route('/reset/<int:user_id>')
 @login_required
@@ -113,6 +123,11 @@ def process(user_id):
         number = data[index]
 
         if number != -1:
+            # Log the drink
+            new_drink = UserDrinks(user_id=user_id, drink_type=drink_type, drink_size=drink_size, timestamp=datetime.now())
+            db.session.add(new_drink)
+
+            # Update user caffeine counter
             user.caffeine += number
             db.session.commit()
 
