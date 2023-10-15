bffd128a558c981bbdeea19d7cd9728ff1cb9f4d
solbell2 <kelseysun13@gmail.com>
2023-10-14 21:40:58 -0700

    Added new features
    Implemented logout and user-specific caffeine tracker and deleted useless variables used for testing

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,12 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    caffeine = db.Column(db.Integer, nullable=False, default=0)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.caffeine = 0
 
 login_manager = LoginManager()
 login_manager.init_app(app)
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,6 @@
 from flask import render_template, session, redirect, url_for, request, flash
-from flask_login import login_user, login_required, login_manager
+from flask_login import login_user, login_required, login_manager, logout_user
 from app import app, coffee_data, db, User
-
-# Initialize the counter on the first request
-@app.before_request
-def before_request():
-    if 'counter' not in session:
-        session['counter'] = 0
 
 # Define a route for the homepage
 @app.route('/')
@@ -16,11 +10,6 @@ def homepage():
 @app.route('/index')
 def index():
     return render_template('index.html')
-
-# Define a route for the about page
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 # Create routes for login and registration
 @app.route('/login', methods=['GET', 'POST'])
@@ -33,11 +22,17 @@ def login():
         if user and user.password == password:
             login_user(user)
             flash('Login successful.', 'success')
-            return redirect(url_for('index'))
+            return redirect(url_for('counter', user_id=user.id))
         else:
             flash('Login failed. Please try again.', 'error')
 
     return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('homepage'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -59,9 +54,9 @@ def register():
     return render_template('register.html')
 
 # Define a route for counter
-@app.route('/counter')
+@app.route('/counter/<int:user_id>')
 @login_required
-def counter():
+def counter(user_id):
     hot_cold_options = ["Hot", "Cold"]
     drink_type_options = [
         "Americano", "Latte", "Flat white", "Cold brew", "Shaken espresso", 
@@ -70,18 +65,22 @@ def counter():
         "Frappuccino", "Creme Frappuccino"
     ]
     drink_size_options = ["Short", "Tall", "Grande", "Venti", "Trenta"]
+    user = User.query.get(user_id)
 
-    return render_template('counter.html', counter=session['counter'], hot_cold_options=hot_cold_options, drink_type_options=drink_type_options, drink_size_options=drink_size_options)
+    return render_template('counter.html', user_id=user.id, counter=user.caffeine, hot_cold_options=hot_cold_options, drink_type_options=drink_type_options, drink_size_options=drink_size_options)
 
-@app.route('/reset')
+@app.route('/reset/<int:user_id>')
 @login_required
-def reset():
-    session['counter'] = 0
-    return redirect(url_for('counter'))
+def reset(user_id):
+    user = User.query.get(user_id)
+    user.caffeine = 0
+    db.session.commit()
+    return redirect(url_for('counter', user_id=user.id))
 
-@app.route('/process', methods=['POST'])
+@app.route('/process/<int:user_id>', methods=['POST'])
 @login_required
-def process():
+def process(user_id):
+    user = User.query.get(user_id)
     hot_cold = request.form.get('hot_cold')
     drink_type = request.form.get('drink_type')
     drink_size = request.form.get('drink_size')
@@ -114,8 +113,9 @@ def process():
         number = data[index]
 
         if number != -1:
-            session['counter'] += number
+            user.caffeine += number
+            db.session.commit()
 
-        return redirect(url_for('counter'))
+        return redirect(url_for('counter', user_id=user.id))
 
 
